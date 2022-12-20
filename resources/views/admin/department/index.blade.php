227e414b4b24b595ebb9b7e71c1aad4ef68dc49d
Ahsanul Hasan <ahsanulhasan599@gmail.com>
2022-12-20 10:25:31 +0600

    Department index change.

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -25,8 +25,9 @@
                                     <th scope="col">ID</th>
                                     <th scope="col">Department Name</th>
                                     <th scope="col">Description</th>
-                                    <th scope="col">Department</th>
-                                    <th scope="col">Category</th>
+                                    <th scope="col">Facebook</th>
+                                    <th scope="col">Twiter</th>
+                                    <th scope="col">Linkdin</th>
                                     <th scope="col">Image</th>
                                     <th scope="col">Action</th>
                                 </tr>
@@ -37,17 +38,36 @@
                                     <td>{{ $loop->index + 1 }}</td>
                                     <td>{{ $department->name }}</td>
                                     <td>{{ $department->description }}</td>
-                                    <td>October 15, 2021</td>
-                                    <td>October 15, 2021</td>
-                                    <td>October 15, 2021</td>
+                                    <td>
+                                        @php
+                                            $social_link = $department->social_link;
+                                            $facebook = json_decode($social_link);
+                                            echo $facebook->facebook;
+                                        @endphp
+                                    </td>
+                                    <td>
+                                        @php
+                                            $social_link = $department->social_link;
+                                            $twiter = json_decode($social_link);
+                                            echo $twiter->twiter;
+                                        @endphp
+                                    </td>
+                                    <td>
+                                        @php
+                                            $social_link = $department->social_link;
+                                            $linkdin = json_decode($social_link);
+                                            echo $linkdin->linkdin;
+                                        @endphp
+                                    </td>
                                     <td>$2,300</td>
                                     <td>
-                                        {{-- <a href="{{ route('news.edit') }}" class="btn btn-success btn-sm waves-effect waves-light"><i class="ri-edit-2-fill"></i></a>
-                                        <a href="{{ route('news.show') }}" class="btn btn-info btn-sm btn-icon waves-effect waves-light"><i class="ri-check-double-line"></i></a>
-                                        <form action="{{ route('news.destroy') }}" method="post">
-                                            <button type="submit" class="btn btn-danger btn-sm btn-icon waves-effect waves-light"><i class="ri-delete-bin-5-line"></i></button>
-                                        </form> --}}
-
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <a href="{{ route('department.edit', $department->id) }}" class="btn btn-success btn-icon waves-effect waves-light"><i class="ri-edit-2-fill"></i></a>
+                                            <a href="{{ route('department.show', $department->id) }}" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-check-double-line"></i></a>
+                                            <form action="{{ route('department.destroy', $department->id) }}" method="post">
+                                                <button type="submit" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-delete-bin-5-line"></i></button>
+                                            </form>
+                                        </div>
                                     </td>
                                 </tr>
                                 @endforeach
