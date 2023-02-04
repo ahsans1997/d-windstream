e0c4bc0f766a99f498144302a2f3f273fcd2a780
ahsans1997 <ahsanulhasan599@gmail.com>
2023-02-04 22:02:04 +0600

    style & border change.

--- resources/views/governingbody.blade.php
+++ resources/views/governingbody.blade.php
@@ -6,14 +6,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12 mb-4">
-                    <div class="card border-primary">
+                    <div class="card">
                         <div class="card-body">
                             <span class="badge badge-success" style="position: absolute; margin-top: -30px;">Chairman</span>
                             <div class="container">
                                 <div class="row">
                                     @foreach ($chairmans as $chairman)
                                         <div class="col-md-6">
-                                            <div class="card border-primary" style="padding: 10px">
+                                            <div class="card" style="padding: 10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 3px 15px 5px;">
                                                 <div class="row">
                                                     <div class="col-md-3">
                                                             <img src="{{ $chairman->getFirstMediaUrl('governingbody') }}" style="border: 1px solid gray; width:100px; height:130px" alt="">
@@ -32,14 +32,14 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card border-primary mt-4">
+                    <div class="card mt-4">
                         <div class="card-body">
                             <span class="badge badge-info" style="position: absolute; margin-top: -30px;">Member</span>
                             <div class="container">
                                 <div class="row">
                                     @foreach ($members as $member)
                                         <div class="col-md-6">
-                                            <div class="card border-primary" style="padding: 10px">
+                                            <div class="card mb-3" style="padding: 10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 3px 15px 5px;">
                                                 <div class="row">
                                                     <div class="col-md-3">
                                                             <img src="{{ $member->getFirstMediaUrl('governingbody') }}" style="border: 1px solid gray; width:100px; height:130px" alt="">
@@ -58,14 +58,14 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card border-primary mt-4">
+                    <div class="card mt-4">
                         <div class="card-body">
                             <span class="badge badge-warning" style="position: absolute; margin-top: -30px;">Secretary</span>
                             <div class="container">
                                 <div class="row">
                                     @foreach ($secretarys as $secretary)
                                         <div class="col-md-6">
-                                            <div class="card border-primary" style="padding: 10px">
+                                            <div class="card" style="padding: 10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 3px 15px 5px;">
                                                 <div class="row">
                                                     <div class="col-md-3">
                                                             <img src="{{ $secretary->getFirstMediaUrl('governingbody') }}" style="border: 1px solid gray; width:100px; height:130px" alt="">
